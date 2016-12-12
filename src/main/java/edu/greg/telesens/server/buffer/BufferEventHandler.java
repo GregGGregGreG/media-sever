df790b97a50ae7de1b91fac72ad43edaed36743b
GreG <truegreg13@mail.ru>
2016-12-12 14:31:45 +0200

    Add comment

--- src/main/java/edu/greg/telesens/server/buffer/BufferEventHandler.java
+++ src/main/java/edu/greg/telesens/server/buffer/BufferEventHandler.java
@@ -4,7 +4,7 @@ package edu.greg.telesens.server.buffer;
  * Created by Phoenix on 11.12.2016.
  */
 public interface BufferEventHandler {
-//    handler event
+    //    handler event for generations packet
     void handle(String sessionId, int packetCount);
 
     void bufferStopped();
